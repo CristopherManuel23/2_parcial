0075bc429152ac4dbc3db76d1a0163e450b0ab79
brand <brand@CRISTOPHE_M_L>
2024-07-12 16:20:52 -0400

    Agregar archivos de proyecto.

---
+++ 2_parcial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace segundo_parcial
+{
+    public class MaterialBibliografico
+    {
+        public string Titulo;
+        public string Autor;
+
+        public virtual void MostrarInfo()
+        {
+            Console.WriteLine($"Título: {Titulo}, Autor: {Autor}");
+        }
+    }
+
+    public class Libro : MaterialBibliografico
+    {
+        public int NumeroDePaginas;
+
+
+        public override void MostrarInfo()
+        {
+            Console.WriteLine($"Libro - Título: {Titulo}, Autor: {Autor}, Páginas: {NumeroDePaginas}");
+        }
+    }
+
+    public class Revista : MaterialBibliografico
+    {
+        public int NumeroDeEdicion;
+
+        public override void MostrarInfo()
+        {
+            Console.WriteLine($"Libro - Título: {Titulo}, Autor: {Autor}, Páginas: {NumeroDeEdicion}");
+        }
+
+    }
+
+}
+
+
